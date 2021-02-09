2df5d7521e871e426281fe8f1f2783b7188e0e5e
alyssa andres <alyssaandres1@gmail.com>
2021-02-08 21:02:00 -0500

    added ids to html page and a function for grabbing the values of the inputs on a click.'

---
+++ contact.js
@@ -0,0 +1,16 @@
+$( ).ready(function() {
+    
+    
+    $("#submitbtn").click(function(){
+        console.log($("#name").val())
+        console.log($("#email").val())
+        console.log($("#Message").val())
+        
+    })
+
+
+
+
+
+});
+
