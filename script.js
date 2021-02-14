93f23eb91421526caae4ddce99de7784f54ce26e
alyssa andres <alyssaandres1@gmail.com>
2021-02-13 22:16:05 -0500

    added functino for showing/hiding porfolio card

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const projectList = document.getElementById('plist');
+$( document ).ready(function() {
+    console.log( "document loaded" );
+});
+
+
 
 // function to call functions on clicks
-projectList.onclick = function () {
+$(".pbtn").click(function () {
+    console.log($(this).text())
+    displayCard();
+});
+
+$(".pbtn").mouseover(function () {
+    displayCard();
+});
+
+// hides card once you leave projects. I want to make this less dramatic, but I want the card to go away more. 
+projects.onmouseleave = function () {
     console.log("yes")
+    pcard.style.display = "none";
 };
+
 
 // array for populating cards
 const projectArray = [
@@ -35,3 +51,20 @@ const projectArray = [
     },
 
 ]
+// borrowed from previous project
+//  initially hides card
+pcard.style.display = "none";
+
+// fuction to display the Quiz card on click
+function displayCard() {
+  console.log("Displaycard function ran");
+  // once the start button is clicked, sets the card to its initial state
+  if (pcard.style.display === "none") {
+    pcard.style.display = "initial";
+  }
+//   // if start button is clicked again, will rehide the card
+//   else {
+//     pcard.style.display = "none";
+//   }
+}
+
