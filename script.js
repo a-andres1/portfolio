70f6499bd828b21f46d4824c7ff9efff8af68a97
alyssa andres <alyssaandres1@gmail.com>
2021-02-13 23:31:50 -0500

    found a way to dynamically add group members github profiles in function

--- script.js
+++ script.js
@@ -28,15 +28,33 @@ projects.onmouseleave = function () {
 
 function writeCard(proj) {
 if (proj === projectArray[0].title){
-    $("#pjpic").attr("src", projectArray[0].pics);
+    $("#pjpic").attr({src: projectArray[0].pics, alt: projectArray[0].alt});
+    $("#ctitle").text(projectArray[0].title);
+    $("#ex").text(projectArray[0].explanation);
+    $("#deployed").attr("href", projectArray[0].deploy);
+    $("#repo").attr("href",projectArray[0].repo);
+    $("#tyler").text("Tyler Gilbert").attr("href", "https://github.com/Tylergilbert913")
+    $("#deepali").text("Deepali Garg").attr("href", "https://github.com/Deepali-Gtech")
+
 }
 else if (proj === projectArray[1].title){
-    $("#pjpic").attr("src", projectArray[1].pics);
+    $("#pjpic").attr({src: projectArray[1].pics, alt: projectArray[1].alt}); 
+    $("#ctitle").text(projectArray[1].title);
+    $("#ex").text(projectArray[1].explanation);
+    $("#deployed").attr("href", projectArray[1].deploy);
+    $("#repo").attr("href",projectArray[1].repo);
+    $("#tyler").text("");
+    $("#deepali").text("");
 
 }
 else if (proj === projectArray[2].title){
-    $("#pjpic").attr("src", projectArray[2].pics);
-
+    $("#pjpic").attr({src: projectArray[2].pics, alt: projectArray[2].alt}); 
+    $("#ctitle").text(projectArray[2].title);
+    $("#ex").text(projectArray[2].explanation);
+    $("#deployed").attr("href", projectArray[2].deploy);
+    $("#repo").attr("href",projectArray[2].repo);
+    $("#tyler").text("");
+    $("#deepali").text("");
 }
 
 
@@ -48,7 +66,8 @@ const projectArray = [
     {
         title: "Feed Me™",
         pics: "./Assets/feed-me.png",
-        explanation: "Words",
+        alt: "screenshot of my project",
+        explanation: "For this project I worked with two other group members. With our app, we tried to help people complete the ardous task of picking a restaurant for dinner. Start by picking your cuisine, enter your address, enter your friends address, and Feed Me will find a restaurant for you.",
         repo: "https://github.com/a-andres1/feed-me-tm",
         deploy: "https://a-andres1.github.io/feed-me-tm/",
 
@@ -57,7 +76,8 @@ const projectArray = [
     {
         title: "Daily Planner",
         pics: "./Assets/CalendarApp.png",
-        explanation: "words",
+        alt: "screenshot of my project",
+        explanation: "For this project I set myself the task of generating everything through jQuery. I knew that everything in this project could be completed using jQuery, it would just take a little more thinking. I'm proud of completing this with minimal HTML.",
         repo: "https://github.com/a-andres1/fun-time-calendar-app",
         deploy: "https://a-andres1.github.io/fun-time-calendar-app/",
 
@@ -66,7 +86,8 @@ const projectArray = [
     {
         title: "Weather Dashboard",
         pics: "./Assets/weatherchecker.png",
-        explanation: "Words",
+        alt: "screenshot of my project",
+        explanation: "For this project I worked with the OpenWeather API to create a dashboard to check the weather in any given city.",
         repo: "https://github.com/a-andres1/weather-checker",
         deploy: "https://a-andres1.github.io/weather-checker/",
 
